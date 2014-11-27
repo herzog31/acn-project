0ca8b450c0920977d6e70a60c13f355ff9bab803
Markus Dausch <markusdausch@outlook.com>
2014-11-27 16:26:15 +0100

    Style refinements, more comments

--- 2 Measurement Suite/http_client.py
+++ 2 Measurement Suite/http_client.py
@@ -8,19 +8,23 @@ import argparse
 import timeit
 from socket import socket, AF_INET, SOCK_STREAM, IPPROTO_TCP
 
-class http_client(object):
-    '''
-    This class represents a simple HTTP client which tries to get a web page from the HTTP server and two images.
-    '''
 
+class HttpClient(object):
+
+    '''
+    This class represents a simple HTTP client which tries to get a web page
+    and two images from a simple HTTP server.
+    '''
 
     def __init__(self, server_ip, server_port):
         '''
-        Constructor for storing the IPv4-address of the server and the port number
+        Constructor for storing the IPv4-address of the server and the port
+        number
         '''
+        # Store the IPv4-address and the port number
         self.server_ip = server_ip
         self.server_port = server_port
-        
+
     def socket_init(self):
         '''
         Initializes the socket for using an IPv4-address and TCP
@@ -28,72 +32,98 @@ class http_client(object):
         # IPv4-address: AF_INET
         # TCP: SOCK_STREAM and IPPROTO_TCP
         self.socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-        
+
     def socket_connect(self):
         '''
-        Connects to the server with the defined IPv4-address with the given port
+        Connects to the server with the defined
+        IPv4-address with the given port
         '''
+        # Connect to the server using the IPv4-address and the port number
         self.socket.connect((self.server_ip, self.server_port))
-        print "Client connected to server", self.server_ip, "on port", self.server_port
-        
+
+        print "Client connected to server", self.server_ip, \
+            "on port", self.server_port
+
     def socket_disconnect(self):
         '''
-        Disconnects from the server
+        Disconnect from the server
         '''
+        # Close the connection and hence the socket
         self.socket.close()
         print "Client disconnected from server", self.server_ip
-        
+
     def fetch_web_page(self):
         '''
         Client requests the web page with its two images
         '''
+
+        # GET-request for the root web page
         self.socket.send("GET / HTTP/1.1\nHost:  fakeserver.org")
+        # Receiving the root web page
         self.buffer1 = self.socket.recv(1024)
         print "Root web page received from server"
-        
+
+        # GET-request for the first image "funny_cat.png"
         self.socket.send("GET /funny_cat.png HTTP/1.1\nHost:  fakeserver.org")
+        # Receiving the first image
         self.buffer2 = self.socket.recv(4096)
         print "funny_cat.png received from server"
-        
+
+        # GET-request for the second image "sad_cat.png"
         self.socket.send("GET /sad_cat.png HTTP/1.1\nHost:  fakeserver.org")
+        # Receiving the second image
         self.buffer3 = self.socket.recv(8192)
         print "sad_cat.png received from server"
-        
+
+
 def main():
     '''
-    This is the main function that first parses the passed arguments (IP-address and server_port)
-    and then creates and starts the HTTP server.
+    This is the main function that first parses the passed arguments
+    (IP-address and server_port) and then creates and starts the HTTP server.
     '''
     # Initialize argument parser
     parser = argparse.ArgumentParser()
-    # Add argument for setting the IPv4-address. Set localhost as default in case no IP-address is passed to the script
-    parser.add_argument("--server_ip", help="IPv4-address of the HTTP-server to which the client should connect.", type=str, default="localhost")
-    # Add argument for setting the server_port. Set 10080 (http normally uses server_port 80) as default in case no server_port number is passed to the script
-    parser.add_argument("--server_port", help="Port of the HTTP-server to which the client should connect. DEFAULT 10080", type=int, default=8080)
+    # Add argument for setting the IPv4-address. Set localhost as default in
+    # case no IP-address is passed to the script
+    parser.add_argument(
+        "--server_ip",
+        help="IPv4-address of the HTTP-server to which the client should connect.",
+        type=str,
+        default="localhost")
+    # Add argument for setting the server_port. Set 10080 (http normally uses
+    # server_port 80) as default in case no server_port number is passed to
+    # the script
+    parser.add_argument(
+        "--server_port",
+        help="Port of the HTTP-server to which the client should connect. DEFAULT 10080",
+        type=int,
+        default=10080)
     # Finally parse the passed arguments
     args = parser.parse_args()
 
-    # Create HTTP client (not part of the time measurement
-    client = http_client(args.server_ip, args.server_port)
-    
+    # Create HTTP client (not part of the time measurement)
+    client = HttpClient(args.server_ip, args.server_port)
+
     # Start measurement
     start_time = timeit.default_timer()
 
+    # Initialize the socket
     client.socket_init()
+    # Connect to the server
     client.socket_connect()
-
     # Fetch the web page
     client.fetch_web_page()
-
+    # Disconnect from the server
     client.socket_disconnect()
-    
+
     # Finish measurement
     end_time = timeit.default_timer()
-    
-    # Calculate and show execution time
-    execution_time = (end_time - start_time) * 1000  # in ms
+
+    # Calculate and show execution time in ms
+    execution_time = (end_time - start_time) * 1000
     print "Execution time was", str(execution_time), "ms"
 
+    print "Client terminated"
+
 if __name__ == '__main__':
-    # Initializing the main function that is called when the script is run
     main()
--- 2 Measurement Suite/http_server.py
+++ 2 Measurement Suite/http_server.py
@@ -7,20 +7,23 @@ Created on 21.11.2014
 import argparse
 from socket import socket, AF_INET, SOCK_STREAM, IPPROTO_TCP
 
-class http_server(object):
+
+class HttpServer(object):
+
     '''
-    This class represents a simple HTTP server which acts as if it would serve one web page and two images.
+    This class represents a simple HTTP server which acts as if it would serve
+    one web page and two images
     '''
 
+    def __init__(self, server_ip, server_port):
+        '''
+        Constructor for storing the IPv4-address of the server and the port
+        number
+        '''
+        # Store the IPv4-address and the port number
+        self.server_ip = server_ip
+        self.server_port = server_port
 
-    def __init__(self, ip, port):
-        '''
-        Constructor for storing the IPv4-address of the server and the port number
-        '''
-        self.server_ip = ip
-        self.server_port = port
-        
-        
     def socket_init(self):
         '''
         Initializes the socket for using an IPv4-address and TCP
@@ -28,102 +31,135 @@ class http_server(object):
         # IPv4-address: AF_INET
         # TCP: SOCK_STREAM and IPPROTO_TCP
         self.socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-        
+
     def socket_bind(self):
         '''
-        Binds the socket to the defined IPv4-address and the server_port-number
+        Binds the socket to the defined IPv4-address and the port-number
         '''
         self.socket.bind((self.server_ip, self.server_port))
         # Accept only one connection at a time
         self.socket.listen(1)
-        
-    def normalize_line_endings(self, request):
-        '''Convert string containing various line endings like \n, \r or \r\n,
-        to uniform \n.'''
-    
-        return ''.join((line + '\n') for line in request.splitlines())   
-    
+
     def disconnect_client(self):
         '''
-        This method disconnects the client from the server
+        Disconnects the client from the server
         '''
-        print "Client disconnected by server", self.client_ip
+        # Closing the connection
         self.connection.close()
+        print "Client disconnected by server", self.client_ip
+
+        # Setting the connection and the IP to None so they can be used for the
+        # next connection
         self.connection = None
         self.client_ip = None
-        
+
     def send_data(self, size):
         '''
-        Sends the passed number of bytes (size) of data (zeros) back to the connected client.
+        Sends the passed number of bytes (size) of data back to the connected
+        client
         '''
+        # Create a byte-array with the given size (in byte)
         data = bytearray(size)
-        print "Sending back data to the client:", str(len(data)), "Byte"
+        print "Sending back", str(len(data)), "byte of data to the client"
+        # Send the data to the client
         self.connection.send(data)
-        
+
     def start(self):
         '''
         Starts the server. The server listens for incoming connections and processes them.
         The server is configured in a way that it only accepts one connection at a time.
         '''
+        # While-True-loop for accepting connections (only one at a time)
         while True:
+            # Waiting for client to connect to the server. Create connection
+            # after client connects and store clients IPv4-address.
             self.connection, self.client_ip = self.socket.accept()
             print "Client connected: ", self.client_ip
-            
+
+            # As long as the client is not disconnected: serve content
             while True:
+                # Waiting for GET-requests from the client
                 self.input_buffer = self.connection.recv(1024)
-                
-                if self.input_buffer:
-                    print "echo input_buffer ->",self.input_buffer
-                
+
+                # Split the lines of the GET-request into arrays (one item for
+                # every line)
                 self.request = self.input_buffer.splitlines()
+
+                # Check whether the request has to lines according to the task
                 if len(self.request) != 2:
+                    # If an empty request is received, then the client does not
+                    # ask for more content. The client can be disconnected If
+                    # more or less then the two lines of a GET-request is
+                    # received, disconnect the client as it violated the
+                    # specification
                     if self.input_buffer == "":
                         print "No more data is received from the client"
                     else:
                         print "Client sent more or less then 2 lines of the GET-request!"
                     self.disconnect_client()
                     break
-                    
+
+                # Check whether the client wants the root web page delivered
                 if self.request[0] == "GET / HTTP/1.1" and self.request[1] == "Host:  fakeserver.org":
                     print "Client requested empty-URL path"
                     self.send_data(1024)
+                # Check whether the client wants the first picture delivered
                 elif self.request[0] == "GET /funny_cat.png HTTP/1.1" and self.request[1] == "Host:  fakeserver.org":
                     print "Client requested funny_cat.png"
                     self.send_data(4096)
+                # Check whether the client wants the second picture delivered
                 elif self.request[0] == "GET /sad_cat.png HTTP/1.1" and self.request[1] == "Host:  fakeserver.org":
                     print "Client requested sad_cat.png"
                     self.send_data(8192)
+                # If none of the requests apply, then the client violated the
+                # specification and will be disconnected
                 else:
                     print "Client violated the specification!"
                     self.disconnect_client()
                     break
-        
+
+        # This part should not be reached as the server always accepts new
+        # connections after the previous one is closed
+        print "Server terminated"
+
 
 def main():
     '''
-    This is the main function that first parses the passed arguments (IP-address and server_port)
-    and then creates and starts the HTTP server.
+    This is the main function that first parses the passed arguments
+    (IP-address and server_port) and then creates and starts the HTTP server.
     '''
-    
+
     # Initialize argument parser
     parser = argparse.ArgumentParser()
-    # Add argument for setting the IPv4-address. Set localhost as default in case no IP-address is passed to the script
-    parser.add_argument("--server_ip", help="IPv4-address at which the HTTP-server is reachable. DEFAULT: localhost", type=str, default="localhost")
-    # Add argument for setting the server_port. Set 10080 (http normally uses server_port 80) as default in case no server_port number is passed to the script
-    parser.add_argument("--server_port", help="Port of the HTTP-server. DEFAULT: 10080", type=int, default=8080)
+    # Add argument for setting the IPv4-address. Set localhost as default in
+    # case no IP-address is passed to the script
+    parser.add_argument(
+        "--server_ip",
+        help="IPv4-address at which the HTTP-server is reachable. DEFAULT: localhost",
+        type=str,
+        default="localhost")
+    # Add argument for setting the server_port. Set 10080 (http normally uses
+    # server_port 80) as default in case no server_port number is passed to
+    # the script
+    parser.add_argument(
+        "--server_port",
+        help="Port of the HTTP-server. DEFAULT: 10080",
+        type=int,
+        default=10080)
     # Finally parse the passed arguments
     args = parser.parse_args()
 
     # Create http_server
-    server = http_server(args.server_ip, args.server_port)
+    server = HttpServer(args.server_ip, args.server_port)
     # Initialize the socket (TCP/IP)
     server.socket_init()
-    # Bind the socket to the defined IP-address and server_port-number
+    # Bind the socket to the defined IP-address and server port-number
     server.socket_bind()
+    
     # Start the server - Accepting connections now
-    print "Starting up server with IP", server.server_ip,"on port", server.server_port
+    print "Starting up HTTP server with IP", server.server_ip, \
+        "on port", server.server_port
     server.start()
 
 if __name__ == '__main__':
-    # Initializing the main function that is called when the script is run
     main()
